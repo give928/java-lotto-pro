52f647b56aa80850959cc80c6259e3d0a8ab5600
joohokim <give928@gmail.com>
2022-05-16 19:14:03 +0900

    feat: 3단계 - 로또(자동) - 구입 금액에 해당하는 로또 매수 구하기

---
+++ src/main/java/lotto/domain/Purchase.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import util.StringUtils;
+
+public class Purchase {
+    private static final int PRICE = 1_000;
+    private static final String ERROR_MESSAGE_INVALID_PURCHASE_MONEY = "구입 금액은 %s원 이상부터 가능합니다.";
+
+    private final int money;
+
+    public Purchase(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (money < PRICE) {
+            throw new IllegalArgumentException(String.format(ERROR_MESSAGE_INVALID_PURCHASE_MONEY, StringUtils.formatThousandsSeparators(PRICE)));
+        }
+    }
+
+    public int count() {
+        return money / PRICE;
+    }
+}
---
+++ src/test/java/lotto/domain/PurchaseTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PurchaseTest {
+    @DisplayName("구입 금액에 해당하는 로또 매수를 반환한다.")
+    @ParameterizedTest(name = "{displayName} money={0}, count={1}")
+    @CsvSource(value = {"1000 1", "1500 1", "2000 2"}, delimiterString = " ")
+    void createPurchase(int money, int count) {
+        // given
+
+        // when
+        Purchase purchase = new Purchase(money);
+
+        // then
+        assertThat(purchase.count()).isEqualTo(count);
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1,000원 미만이면 IllegalArgumentException 이 발생한다.")
+    void thrownByInvalidMoney() {
+        // given
+        int money = 999;
+
+        // when
+        ThrowableAssert.ThrowingCallable throwingCallable = () -> new Purchase(money);
+
+        // then
+        assertThatThrownBy(throwingCallable).isInstanceOf(IllegalArgumentException.class);
+    }
+}
