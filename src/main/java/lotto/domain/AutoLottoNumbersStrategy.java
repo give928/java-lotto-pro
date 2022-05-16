c74cc34b9a81e34a75f33abe58ca61c7c39d2f6b
joohokim <give928@gmail.com>
2022-05-16 18:00:56 +0900

    feat: 3단계 - 로또(자동) - 자동 로또 번호 생성

---
+++ src/main/java/lotto/domain/AutoLottoNumbersStrategy.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class AutoLottoNumbersStrategy implements LottoNumbersStrategy {
+    private final int startNumber;
+    private final int endNumber;
+    private final int numberSize;
+
+    public AutoLottoNumbersStrategy(int startNumber, int endNumber, int numberSize) {
+        this.startNumber = startNumber;
+        this.endNumber = endNumber;
+        this.numberSize = numberSize;
+    }
+
+    @Override
+    public List<Integer> generate() {
+        List<Integer> rangeNumbers = generateRangeNumbers(startNumber, endNumber);
+        Collections.shuffle(rangeNumbers);
+        List<Integer> shuffleNumbers = new ArrayList<>(rangeNumbers.subList(0, numberSize));
+        Collections.sort(shuffleNumbers);
+        return shuffleNumbers;
+    }
+
+    private static List<Integer> generateRangeNumbers(int startNumber, int endNumber) {
+        return IntStream.rangeClosed(startNumber, endNumber)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
